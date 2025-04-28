28ee274c2cdc0be0a42b6825ce6680f53cf6581b
Manoj-Mahat0 <codeinlastbench@gmail.com>
2025-04-28 15:38:01 +0530

    New

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 import os
 
 app = FastAPI()
 
-
+# Allow CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://contact-u87u.onrender.com"],  # your frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],   # or ["POST", "GET"] if you want to restrict
+    allow_headers=["*"],
+)
 
 
 
